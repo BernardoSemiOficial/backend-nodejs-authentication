de888ea2327a790400adb8e0ddfa643809e351bf
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-09-30 21:49:44 -0300

    feat: adicionado endpoint de sso via github

--- src/modules/authentication/controller/authentication.controlller.ts
+++ src/modules/authentication/controller/authentication.controlller.ts
@@ -1,8 +1,10 @@
+import axios, { AxiosError } from "axios";
 import { randomUUID } from "crypto";
 import { Response, Router } from "express";
 import { tokenService, userService } from "../../..";
 import { TypedRequestBody } from "../../../interfaces/request";
 import {
+  AuthGithubPayload,
   LoginPayload,
   RefreshTokenPayload,
   RegisterPayload,
@@ -77,6 +79,67 @@ authenticationRouter.post(
       isAccessToken: true,
     });
     res.json({ accessToken }).status(201);
+  }
+);
+
+authenticationRouter.post(
+  "/api/auth/github",
+  async (req: TypedRequestBody<AuthGithubPayload>, res: Response) => {
+    const { code } = req.params;
+    let tokenGithub = null;
+
+    try {
+      tokenGithub = await axios.post<{
+        access_token: string;
+        token_type: string;
+        scode: string;
+      }>("https://github.com/login/oauth/access_token", {
+        client_id: process.env.GITHUB_OAUTH_CLIENT_ID,
+        client_secret: process.env.GITHUB_OAUTH_SECRET_KEY,
+        code,
+      });
+    } catch (error) {
+      return res.status(400).json({ message: "Intern error" });
+    }
+
+    console.log(tokenGithub.data);
+    if (!tokenGithub) {
+      return res.status(401).json({ message: "Error to get token" });
+    }
+
+    const accessToken = tokenGithub.data.access_token;
+    const tokenType = tokenGithub.data.token_type;
+
+    let userGithub = null;
+    try {
+      userGithub = await axios.get<{ login: string }>(
+        "https://api.github.com/user",
+        {
+          headers: {
+            Accept: "application/json",
+            Authorization: `${tokenType} ${accessToken}`,
+          },
+        }
+      );
+    } catch (error) {
+      console.log((error as AxiosError).message);
+      return res.status(401).json({ message: "Intern error" });
+    }
+
+    console.log(userGithub.data);
+
+    if (!userGithub) {
+      return res.status(401).json({ message: "Error to get token" });
+    }
+
+    userService.createUser({
+      id: randomUUID(),
+      email: "",
+      name: userGithub.data.login,
+      password: "",
+    });
+
+    return res.json({ user: userGithub.data, accessToken, tokenType });
   }
 );
 
--- src/modules/authentication/interfaces/authentication.interface.ts
+++ src/modules/authentication/interfaces/authentication.interface.ts
@@ -18,3 +18,7 @@ export interface RegisterResponse {
 export interface RefreshTokenPayload {
   refreshToken: string;
 }
+
+export interface AuthGithubPayload {
+  code: string;
+}
